a333cd3adad6a60115f4c9537ff6a301386413be
Raimond <raimond.bogdiun@gmail.com>
2018-04-13 02:34:38 +0300

    Revert "Initial commit"
    This reverts commit ff4da9a
    
    Signed-off-by: Raimond <raimond.bogdiun@gmail.com>

--- src/main/java/lt/raimond/laivu_musis/Exceptions/InvalidCoordinateException.java
+++
@@ -1,7 +0,0 @@
-package lt.raimond.laivu_musis.Exceptions;
-
-public class InvalidCoordinateException extends Exception {
-    public InvalidCoordinateException(String message) {
-        super(message);
-    }
-}
--- src/main/java/lt/raimond/laivu_musis/Exceptions/InvalidShipPlaceException.java
+++
@@ -1,7 +0,0 @@
-package lt.raimond.laivu_musis.Exceptions;
-
-public class InvalidShipPlaceException extends Throwable {
-    public InvalidShipPlaceException(String invalid_ship) {
-        super(invalid_ship);
-    }
-}
